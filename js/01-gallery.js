fe52f9174022e009ad6ae1cb7dfe2832b8b748a1
lfurmanchuk <lesyazhuruk@gmail.com>
2022-11-21 21:10:48 +0200

    Update task-1, task-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,101 +1,48 @@
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// Створюємо посилання на елемент
-const galleryList = document.querySelector(".gallery");
+const galleryСontainer = document.querySelector('.gallery');
 
-const imagesMarkup = makeGalleryCard(galleryItems);
-
-// Додаємо розмітку в html
-galleryList.insertAdjacentHTML("afterbegin", imagesMarkup);
-
-// Назначаємо слухача на клік
-galleryList.addEventListener("click", onGalleryClick);
-
-// Створюємо елемент за допомогою шаблонного рядка
-// та масив елементів
-function makeGalleryCard (galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
+const galleryItemsMarkup = galleryItems.map(item => {
+  return `
       <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-      <img class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-      />
-      </a>
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+        </a>
       </div>`;
     })
-    .join("");
-}
+    .join('');
 
-function onGalleryClick(event) {
+galleryСontainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+galleryСontainer.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
     // Запобігаємо перезавантаженню сторінки
-    event.preventDefault();
+    e.preventDefault();
     // Робимо перевірку делегування
-    if (event.target.nodeName !== "IMG") {
+    if (e.target.nodeName !== 'IMG') {
     return;
   }
     // Підключаемо відкриття модального вікна за допомогою бібліотеки
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-    instance.show();
-    
-    // Додаємо закриття модального вікна після натискання клавіші Escape
-    galleryList.addEventListener("keydown", (event) => {
-    if (event.code === 'Escape') {
-        instance.close()
+  const instance = basicLightbox.create(`
+    <img src='${e.target.dataset.source}'/>`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', onEscape)
+      },
+      onClose: () => {
+         document.removeEventListener('keydown', onEscape)
+      }
     }
-});
+  );
 
+  instance.show();
+  
+  function onEscape(e) {
+      if (e.key === 'Escape') instance.close();
+    }
 }
-
-// console.log(galleryItems);
-
-// const galleryList = document.querySelector(".gallery");
-// const imagesMarkup = makeGalleryCard(galleryItems);
-
-// galleryList.insertAdjacentHTML("beforeend", imagesMarkup);
-// galleryList.addEventListener('click', onGalleryClick)
-
-// function makeGalleryCard(galleryItems) {
-//     return galleryItems
-//         .map(({ preview, original, description }) => {
-//             return `
-//         <div class="gallery__item">
-//           <a class="gallery__link" href="${original}">
-//             <img
-//             class="gallery__image"
-//             src="${preview}"
-//             data-source="${original}"
-//             alt="${description}"
-//             />
-//           </a>
-//         </div>`;
-//         })
-//         .join('');
-// }
-
-// function onGalleryClick(e) {
-//   e.preventDefault();
-
-//   if (e.target.classList.contains(".gallery")) return;
-//     const source = e.target.dataset.source;
-    
-//   const instance = basicLightbox.create(`
-//     <img src="${source}"width="800" height="600">`);
-
-//     instance.show();
-    
-
-// };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,39 +1,19 @@
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
-// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
-// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
-// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryСontainer = document.querySelector('.gallery');
 
-const galleryList = document.querySelector(".gallery");
-
-const imagesMarkup = makeGalleryCard(galleryItems);
-
-// Додаємо розмітку в html
-galleryList.insertAdjacentHTML("afterbegin", imagesMarkup);
-
-// Створюємо елемент за допомогою шаблонного рядка
-// та масив елементів
-function makeGalleryCard (galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-      <li class="gallery__item">
-      <a class="gallery__link" 
-      href="${original}">
-      <img class="gallery__image" 
-      src="${preview}" 
-      alt="${description}" />
-      </a>
-      </li>`;
+const galleryItemsMarkup = galleryItems.map(item => {
+  return `
+      <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
     })
-    .join("");
-}
+    .join('');
 
-new SimpleLightbox('.gallery a', {
+galleryСontainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
   captionDelay: 250,
   captionsData: 'alt',
 });
